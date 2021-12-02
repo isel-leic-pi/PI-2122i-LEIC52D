d229cdc2225e030dc2d8b9c8730a4ff6220803b1
Miguel Gamboa <miguelgamboa@outlook.com>
2021-12-02 10:25:50 +0000

    setup lesson 25

--- lesson25-tasky-ui-html/index.js
+++ lesson25-tasky-ui-html/index.js
@@ -7,6 +7,18 @@ require('./lib/tasky-routes')(app)
 
 const PORT = 3000
 
-app.listen(PORT, () => {
-    console.log('Listening on port ' + PORT)
-})
+insertDummies()
+    .then(() => app.listen(PORT, () => {
+        console.log('Listening on port ' + PORT)
+    }))
+
+function insertDummies() {
+    const tasks = require('./lib/tasks-in-mem')
+    const prms = [
+        tasks.insertTask('gamboa', 7, 'swim-mile', 'Achieve 1 mile swimming open water.'),
+        tasks.insertTask('gamboa', 3, 'pi-workout', 'Complete the first workout of Web Dev course.'),
+        tasks.insertTask('gamboa', 20, 'peaa', 'Finish the book of Patterns of Enterprise Application Architecture by Martin Fowler.'),
+        tasks.insertTask('rambo', 4, 'room-manage', 'Manage all books and stuff in my room')
+    ]
+    return Promise.all(prms)
+}
